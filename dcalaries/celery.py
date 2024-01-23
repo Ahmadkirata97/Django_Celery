d6fe67c03c04094e25437f06a6995fe26c849e7a
unknown <ahmad.aldon@outlook.com>
2024-01-24 00:31:15 +0300

    prioritizing using redis queues

--- dcalaries/celery.py
+++ dcalaries/celery.py
@@ -4,9 +4,16 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcalaries.settings')
 app = Celery("dcalaries")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {
-    'firstapp.tasks.*': {'queue':'q1'},
-    'firstapp.tasks.task2': {'queue':'q2'},
+# app.conf.task_routes = {
+#     'firstapp.tasks.*': {'queue':'q1'},
+#     'firstapp.tasks.task2': {'queue':'q2'},
+# }
+
+
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_strategy':'priority',
 }
 
 
