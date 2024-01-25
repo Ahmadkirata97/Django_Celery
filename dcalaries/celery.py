784c7a6b0790b91085d2d626ca590511ab6df58c
unknown <ahmad.aldon@outlook.com>
2024-01-25 16:09:20 +0300

    Workers Monitoring and Managing Using flower

--- dcalaries/celery.py
+++ dcalaries/celery.py
@@ -17,25 +17,22 @@ app.conf.worker_prefetch_multiplier = 1 # enable the worker to fetch multiple ta
 app.conf.worker_concurrency = 1 # number of worker threds that celery will reserve to process task 
 
 @app.task(queue='tasks')
-def t1():
-    time.sleep(10)
-    return 
+def t1(a, b, message=None):
+    
+    result = a + b
+    if message:
+        result = f"{message}: {result}"
+    return result
 
 @app.task(queue='tasks')
 def t2():
     time.sleep(10)
-    return 
+    return
 
 @app.task(queue='tasks')
 def t3():
-    time.sleep(10)
-    return 
-
-@app.task(queue='tasks')
-def t4():
-    time.sleep(10)
-    return 
-
+    time.sleep(3)
+    return
 
 # app.conf.task_routes = {
 #     'firstapp.tasks.*': {'queue':'q1'},
@@ -54,3 +51,44 @@ def t4():
 
 
 app.autodiscover_tasks()
+
+def test():
+    # Call the task asynchronously
+    result = t1.apply_async(args=[5,10], kwargs={"message":"The sum is"})
+
+    # Check if the task has completed
+    if result.ready():
+        print("Task has completed")
+    else:
+        print("Task is still running")
+
+    # Check if the task completed successfully
+    if result.successful():
+        print("Task completed successfully")
+    else:
+        print("Task encountered an error")
+
+    # Get the result of the task
+    try:
+        task_result = result.get()
+        print("Task result:", task_result)
+    except Exception as e:
+        print("An exception occurred:", str(e))
+
+    # Get the exception (if any) that occurred during task execution
+    exception = result.get(propagate=False)
+    if exception:
+        print("An exception occurred during task execution:", str(exception))
+
+# Synchronous task execution
+def execute_sync():
+    result = t1.apply_async(args=[5,10], kwargs={"message":"The sum is"})
+    task_result = result.get()
+    print("Task is running synchronously")
+    print(task_result)
+
+# Asynchronous task execution
+def execute_async():
+    result = t1.apply_async(args=[5,10], kwargs={"message":"The sum is"})
+    print("Task is running asynchronously")
+    print("Task ID:", result.task_id)
